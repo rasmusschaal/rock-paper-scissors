9e2929a3d2ed8c32c7d9d5acc45f5ac2b76157e7
rasmusschaal <rasmusschaal@gmail.com>
2024-01-17 09:21:26 +1100

    add comments

--- script.js
+++ script.js
@@ -12,14 +12,22 @@ function checkAnswer(choice) {
 function genReply() {
     var randomNum = Math.floor(Math.random() * (4 - 1) ) + 1;
 
-    //RETURN ROCK IF randomNUM == 1
+    //RETURN ROCK IF randomNum == 1
     if (randomNum == 1) {
         return "rock";
-    } else if (randomNum == 2) {
+    }
+    //RETURN SCISSORS IF randomNum == 2
+    else if (randomNum == 2) {
         return "scissors";
-    } else {
+    }
+    //RETURN PAPER IF randomNum == 3
+    else {
         return "paper";
     }
+}
+
+function checkWinner (play1, play2) {
+
 }
 
 //GET INPUT FOR ANSWER
